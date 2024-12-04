c18cc48892a21231441f0e8a002b09520e1724ff
Sávio Lopes <164911067+saviolvs@users.noreply.github.com>
2024-12-04 15:10:19 -0300

    Update Venda.java

--- src/model/Venda.java
+++ src/model/Venda.java
@@ -1,38 +1,48 @@
 package model;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 
 public class Venda {
 
     private Item[] itens;
-    private LocalDateTime dataHora;
     private int quantidadeItens;
 
-    public Venda(){
+    public Venda() {
         itens = new Item[3];
-        dataHora = LocalDateTime.now();
         quantidadeItens = 0;
     }
 
-    public double getTotal(){
+    public double getTotal() {
         double total = 0;
-        for(int i=0;i<quantidadeItens; i++){
+        for (int i = 0; i < quantidadeItens; i++) {
             total += itens[i].getSubtotal();
         }
         return total;
     }
 
-    public boolean adicionarItem(Item item){
-        if(quantidadeItens == itens.length){
-            itens = Arrays.copyOf(itens, itens.length+3);
+    public boolean adicionarItem(Item item) {
+        if (quantidadeItens == itens.length) {
+            itens = Arrays.copyOf(itens, itens.length + 3);
         }
         itens[quantidadeItens++] = item;
         return true;
     }
 
-    public Item[] getItens(){
-        return Arrays.copyOf(itens, quantidadeItens);
+    public boolean removerItem(int codigo) {
+        for (int i = 0; i < quantidadeItens; i++) {
+            if (itens[i].getCodigo() == codigo) {
+                for (int j = i; j < quantidadeItens - 1; j++) {
+                    itens[j] = itens[j + 1];
+                }
+                itens[quantidadeItens - 1] = null;
+                quantidadeItens--;
+                return true;
+            }
+        }
+        return false;
     }
 
+    public Item[] getItens() {
+        return Arrays.copyOf(itens, quantidadeItens);
+    }
 }
